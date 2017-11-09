deff3df31bd575595f7a9ea7cff314f73fd3f032
chintankhanna <mail4chintan@yahoo.com>
2017-11-09 10:23:16 +0100

    header file

---
+++ test_parser_ZECv2/src/zcash.h
@@ -0,0 +1,13 @@
+//zcash.h   contains declarations etc
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
+#include <math.h>
+#include <sys/time.h>
+#include <time.h>
+#include <openssl/sha.h>
+
+
+#define LITTLEENDIAN 0
+#define BIGENDIAN 1
+#define HEADERSIZE 1487
